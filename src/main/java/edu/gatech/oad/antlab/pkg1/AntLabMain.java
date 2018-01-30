a1656ea02d816b6ed979a8c9437746e3c7996990
Russell Joseph Conklin <rconklin6@gatech.edu>
2018-01-30 02:38:47 -0500

    Changed my name according to instructions and left a note for Austin.

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,7 +57,7 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
+		  Person1 p1 = new Person1("P1"); //Austin, you forgot to replace "P1" with Austin. Also We should decide how we're going to do exactly what I'm doing now. Should we leave a note? Should we just change it?
 		  toPrint += p1.toString("acondict3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
@@ -69,8 +69,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Russell Conklin");
+                  toPrint += p4.toString("rconklin6");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
